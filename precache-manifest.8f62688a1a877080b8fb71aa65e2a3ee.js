ffbac2971ccd9216437ad7fe2be240269f98d4a7
tempname1533 <pedro_de_sousa@outlook.com>
2020-11-18 10:50:07 +0100

    Updates

---
+++ precache-manifest.8f62688a1a877080b8fb71aa65e2a3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004237f8d8b6945e3100b6e54507f918",
+    "url": "/React-Codepen-clone/index.html"
+  },
+  {
+    "revision": "e8b0d756d6466db089de",
+    "url": "/React-Codepen-clone/static/css/2.85c6439a.chunk.css"
+  },
+  {
+    "revision": "1e65dbd0ba5a094c4070",
+    "url": "/React-Codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "e8b0d756d6466db089de",
+    "url": "/React-Codepen-clone/static/js/2.1aa0f0be.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/React-Codepen-clone/static/js/2.1aa0f0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e65dbd0ba5a094c4070",
+    "url": "/React-Codepen-clone/static/js/main.fab5e29f.chunk.js"
+  },
+  {
+    "revision": "9170f35beabba0d55efc",
+    "url": "/React-Codepen-clone/static/js/runtime-main.9cb7aeb0.js"
+  }
+]);
